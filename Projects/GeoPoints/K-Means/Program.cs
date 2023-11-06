449be4d4ba118a35d019d6304ad899a3af62371e
Ubaydullo Asatullaev <Ubaydullohazard@gmail.com>
2023-11-06 15:36:13 +0900

    Update Program.cs

--- Projects/GeoPoints/K-Means/Program.cs
+++ Projects/GeoPoints/K-Means/Program.cs
@@ -25,23 +25,24 @@ class Program
         using var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)); // csvconfiguration is set to use invariantculture 
         // to properly parse of numeric values 
 
+
+        
         var records = csv.GetRecords<GeoPointstest>().ToList(); // GetRecords method is used to read and parse csv file into list of objects 
         // type GeoPointstest object, records contains all data from csv file, records list
-
         var X1 = records.Select(r => new double[] { r.WP_Lat, r.WP_Lon }).ToArray(); // new array X1 created to ontain lat, lon of each data point
 
+        
         int n_clusters = 200; // sets number of clusters to partition the data into
-
         KMeans kmeans = new KMeans(n_clusters); // it creates new KMeans object with specified number of clusters 
         KMeansClusterCollection clusters = kmeans.Learn(X1); // it trains K-means algorithm on data in X1
         // Learn method returns KMeansClusterCollection object that contains centroids of clusters.
 
-        var centroids = clusters.Centroids; // it gets centroids of clusters
 
+        
+        var centroids = clusters.Centroids; // it gets centroids of clusters
         var downsampledData = centroids.Select((centroid, index) =>
         {
             var originalDataPoint = records[index]; // Get the original data point
-
             return new GeoPointstest
             {
                 Type = originalDataPoint.Type,      // Set Type to the original value
